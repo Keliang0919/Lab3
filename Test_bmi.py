0d919346d6c92604cd64e0834c6f1b9c6f4ee45f
Tan Ke Liang <KELIANG.23@ichat.sp.edu.sg>
2024-05-02 22:06:57 +0800

    Changed

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,29 +1,15 @@
 import Lab2.bmi as bmi
 
 def test_bmi_under_weight(bmi):
-    height_input = 1.73
-    weight_input = 57
-    calc_bmi = weight_input/(height_input*height_input)
-    if calc_bmi < 18.5:
-        assert (calc_bmi < 18.5)
-        return
+    assert bmi.calc_bmi(height = 1.79, weight = 57) == -1
+    return -1
 
 
 def test_bmi_normal__weight(bmi):
-    height_input = 1.73
-    weight_input = 57
-    calc_bmi = weight_input/(height_input*height_input)
-    if 18.5 < calc_bmi < 25.0:
-        assert (18.5 < calc_bmi < 25)
-        result1 = print("Normal Weight")
-        return result1
+    assert bmi.calc_bmi(height = 1.79, weight = 67) == 0
+    return 0
 
 
 def test_bmi_over_weight(bmi):
-    height_input = 1.73
-    weight_input = 57
-    calc_bmi = weight_input/(height_input*height_input)
-    if bmi > 25.0:
-        assert (calc_bmi> 25.0)
-        result2 = print("Overweight")
-        return result2
+    assert bmi.calc_bmi(height = 1.79, weight = 87) == 1
+    return 1
