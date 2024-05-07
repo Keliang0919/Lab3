51c92a0246c83acc02a6cd1046965200e1448677
Tan Ke Liang <KELIANG.23@ichat.sp.edu.sg>
2024-05-07 11:31:29 +0800

    Changed

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,15 +1,20 @@
 import Lab2.bmi as bmi
 
-def test_bmi_under_weight(bmi):
-    assert bmi.calc_bmi(height = 1.79, weight = 57) == -1
-    return -1
+def test_bmi_under_weight():
+    height_input = 1.79
+    weight_input = 47
+    result = bmi.calculate_bmi(height_input, weight_input)
+    expected_result = -1
+    assert (result == expected_result)
 
 
-def test_bmi_normal__weight(bmi):
-    assert bmi.calc_bmi(height = 1.79, weight = 67) == 0
-    return 0
+def test_bmi_normal__weight():
+    result = bmi.calculate_bmi(1.79, 60)
+    expected_result = 0
+    assert (result == expected_result)
 
 
-def test_bmi_over_weight(bmi):
-    assert bmi.calc_bmi(height = 1.79, weight = 87) == 1
-    return 1
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.79, 97)
+    expected_result = 1
+    assert (result == expected_result)
